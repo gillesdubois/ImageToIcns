6655d2076a8933c02f08add16dbcee01d4e558f0
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-08 18:13:48 +0200

    Update infos (add copyright)

---
+++ ImageToIcns/ImageProcess.swift
@@ -0,0 +1,13 @@
+//
+//  ImageProcess.swift
+//  ImageToIcns
+//
+//  Created by Gilles Dubois on 08/10/2017.
+//  Copyright © 2017 Gilles Dubois. All rights reserved.
+//
+
+import Cocoa
+
+class ImageProcess: NSObject {
+
+}
