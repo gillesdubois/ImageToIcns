2b816cdc6b360b9abd1b83c0e7e8fb7da45cf49b
Gilles Dubois <gilles.dubois18@icloud.com>
2017-10-08 14:00:40 +0200

    Initial Commit

---
+++ ImageToIcns/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageToIcns
+//
+//  Created by Gilles Dubois on 08/10/2017.
+//  Copyright © 2017 Gilles Dubois. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ImageToIcns/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageToIcns
+//
+//  Created by Gilles Dubois on 08/10/2017.
+//  Copyright © 2017 Gilles Dubois. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
