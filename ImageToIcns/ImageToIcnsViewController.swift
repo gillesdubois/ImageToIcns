3f815156660499e3d9cb1ea1edb35aa60782ca0a
Gilles Dubois <gilles.dubois@factorfx.com>
2017-10-09 19:36:33 +0200

    Add iconset processing to icns

--- ImageToIcns/ImageToIcnsViewController.swift
+++ ImageToIcns/ImageToIcnsViewController.swift
@@ -10,8 +10,9 @@ import Cocoa
 
 class ImageToIcnsViewController: NSViewController {
     
-    var destFolder: URL?
-    var sourcePath: String?
+    var destFolder: URL!
+    var sourcePath: String!
+    var fileName: String! = "output_file"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +40,44 @@ class ImageToIcnsViewController: NSViewController {
                 self.destFolder = panel.urls[0]
                 
                 // Process image
-                print ("Destination folder : \(self.destFolder?.path ?? "none")")
+                print ("Source folder : \(self.sourcePath ?? "none")")
                 
                 // Transform source to icns
+                let task = Process()
+
+                // Create a Task instance
+                
+                let output = "\(self.destFolder!.path)/\(self.fileName!).icns"
+                
+                print ("Destination folder : \(output)")
+                
+                // Set the task parameters
+                task.launchPath = "/usr/bin/iconutil"
+                task.arguments = ["-c", "icns", "\(self.sourcePath!)", "--output", "\(output)"]
+                
+                // Create a Pipe and make the task
+                // put all the output there
+                let pipe = Pipe()
+                task.standardOutput = pipe
+                
+                // Launch the task
+                task.launch()
+
+                // Trigger notification
+                self.showNotification()
+  
                 
             }
         }
 
+    }
+    
+    private func showNotification() -> Void {
+        let notification = NSUserNotification()
+        notification.title = "Success !"
+        notification.informativeText = "Your iconset has been transformed to icns"
+        notification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.deliver(notification)
     }
 
 }
